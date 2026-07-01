a2a06e6bc1055c72b8fd00e59c30e0cbc1d5681d
Dung DANG <minhdung.dang@gmail.com>
2026-07-01 23:32:06 +0700

    langgraph for sentiment analysis

--- 01-langgraph-started/02-sentiment-analysis-agent.py
+++ 01-langgraph-started/02-sentiment-analysis-agent.py
@@ -2,6 +2,10 @@ from typing_extensions import TypedDict, Literal
 from langgraph.graph import StateGraph, START, END
 from langchain_ollama import ChatOllama
 from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.output_parsers import (
+    StrOutputParser
+)
+
 from pydantic import BaseModel, Field
 
 from langfuse.langchain import CallbackHandler
@@ -23,3 +27,88 @@ llm = ChatOllama(
 
 langfuse_callback_hdl = CallbackHandler()
 
+config = {"callbacks": [langfuse_callback_hdl]}
+
+class SentimentAnalysis(BaseModel):
+    sentiment: Literal['positive', 'negative'] = Field(description="The classified sentiment, either positive or negative")
+    confidence: float = Field(ge=0, le=1.0, description="Confidence score of the classification, from 0.0 to 1.0")
+    reason: str = Field(description="Brief explanation")
+
+class SentimentState(TypedDict):
+    original_tweet: str
+    sentiment: str
+    confidence: float
+
+    response_tweet: str
+
+def analyze_sentiment(in_state: SentimentState) -> SentimentState:
+    structured_llm = llm.with_structured_output(SentimentAnalysis)
+
+    messages = [
+        SystemMessage("Analyze the sentiment and provide the structured output. Use 0.0 to 1.0 scale for confidence. Higher score is for highly confident about the sentiment classification (highly positive or highly negative). Score zero should be for neutral."),
+        HumanMessage(in_state['original_tweet'])
+    ]
+
+    analysis = structured_llm.invoke(messages)
+
+    return {
+        'sentiment': analysis.sentiment,
+        'confidence': analysis.confidence
+    }
+
+def generate_positive_response(in_state: SentimentState) -> SentimentState:
+    messages = [
+        SystemMessage(f"""Generate a warm response to this positive tweet under 250 chars.
+        Confidence: {in_state['confidence']}.
+        Confidence is scored from 0 to 1.0. High confidence means be enthusiastic, otherwise be fair enough.
+        """),
+        HumanMessage(in_state['original_tweet'])
+    ]
+
+    chain = llm | StrOutputParser()
+
+    return {'response_tweet': chain.invoke(messages)}
+
+def generate_negative_response(in_state: SentimentState) -> SentimentState:
+    messages = [
+        SystemMessage(f"""Generate an empathetic response to this positive tweet under 250 chars.
+        Confidence: {in_state['confidence']}.
+        Confidence is scored from 0 to 1.0. High confidence means it was very bad and would need support, otherwise be understanding.
+        """),
+        HumanMessage(in_state['original_tweet'])
+    ]
+
+    chain = llm | StrOutputParser()
+
+    return {'response_tweet': chain.invoke(messages)}
+
+def route_by_sentiment(in_state: SentimentState):
+    if in_state['sentiment'] == 'positive':
+        return 'generate_positive_response'
+
+    return 'generate_negative_response'
+
+def create_agent_graph():
+    builder = StateGraph(SentimentState)
+
+    builder.add_node("analyze_sentiment", analyze_sentiment)
+    builder.add_node("generate_positive_response", generate_positive_response)
+    builder.add_node("generate_negative_response", generate_negative_response)
+
+    builder.add_edge(START, "analyze_sentiment")
+
+    builder.add_conditional_edges(
+        "analyze_sentiment",
+        route_by_sentiment,
+        ["generate_positive_response", "generate_negative_response"]
+    )
+    
+    builder.add_edge("generate_positive_response", END)
+    builder.add_edge("generate_negative_response", END)
+
+    return builder.compile()
+
+graph = create_agent_graph()
+# print(graph.invoke(input={'original_tweet': "I have just had dinner."}, config=config))
+print(graph.invoke(input={'original_tweet': "The weather is nice today. I do not know what to do."}, config=config))
+print(graph.invoke(input={'original_tweet': "The world is ridiculous. I am fed up."}, config=config))
